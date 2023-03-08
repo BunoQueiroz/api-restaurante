15923c12997e24943c6532230e26aa7eaeb9f23d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-08 11:27:49 -0300

    Criando os modelos de produto, conta e pedido

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Client(User):
     image = models.ImageField(upload_to='clients/%Y/%m/%d')
@@ -7,3 +8,20 @@ class Client(User):
 
     def __str__(self):
         return self.first_name
+
+class Product(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    description = models.CharField(max_length=150, blank=True)
+    price = models.CharField(max_length=10, blank=False)
+    date_create = models.DateField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    image = models.ImageField(blank=True, upload_to='products/%Y/%m/%d')
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    amount = models.CharField(max_length=2, blank=None, default='1')
+    time = models.DateTimeField(default=timezone.now)
+
+class Account(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
