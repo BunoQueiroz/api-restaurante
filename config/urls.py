d87540adb083fa640b07f84f82ae9d38177041c9
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-09 15:25:11 -0300

    Registrando as rotas dos recursosde conta, pedido e produto

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from clients.views import ClientViewSet
+from clients.views import ClientViewSet, AccountViewSet, OrderViewSet, ProductViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register('clients', ClientViewSet, 'clients')
+router.register('accounts', AccountViewSet, 'accounts')
+router.register('orders', OrderViewSet, 'orders')
+router.register('products', ProductViewSet, 'products')
 
 urlpatterns = [
     path('', include(router.urls)),
