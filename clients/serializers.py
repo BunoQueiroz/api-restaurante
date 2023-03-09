f77580d9f8372ffb2a9876b5ccee5ebcad4cd24b
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-09 15:22:00 -0300

    Desenvovendo as classes serializers de conta, pedido e produto

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,8 +1,22 @@
-from rest_framework import serializers
-from clients.models import Client
+from rest_framework import serializers, generics
+from clients.models import Client, Account, Order, Product
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['first_name', 'last_name', 'email', 'password', 'image', 'is_active', 'cpf']
     
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
