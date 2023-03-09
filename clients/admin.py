a847876015eab6f863cf9487d943fab31a5e79ca
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-09 07:37:48 -0300

    Adicionando os modelos de conta, pedido e produto no site admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from clients.models import Account, Client, Order, Product
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Product)
