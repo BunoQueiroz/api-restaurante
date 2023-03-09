bc843d719999497a1b799697bef5a3bf5aa07d80
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-09 15:24:13 -0300

    Desenvolvendo as classes ViewSets de conta, produto e pedido

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,20 @@
 from rest_framework import viewsets
-from clients.serializers import ClientSerializer
-from clients.models import Client
+from clients.serializers import ClientSerializer, AccountSerializer, OrderSerializer, ProductSerializer
+from clients.models import Client, Account, Order, Product
 
 class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    
